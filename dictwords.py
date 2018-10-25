d2792657d143a731019de0e719718c08f77dc4a9
Lucia Reynoso <rvaicul@gmail.com>
2018-10-24 21:14:14 -0700

    remove benchmarking

--- dictwords.py
+++ dictwords.py
@@ -20,7 +20,6 @@ def random_words(num_words):
     # read through the dictionary
     for number in lucky_numbers:
         words += linecache.getline('/usr/share/dict/words', number)
-        
 
     return words
 
@@ -29,7 +28,3 @@ if __name__ == '__main__':
     output = random_words(num_words=num_words)
     output = output.replace('\n', ' ')
     print(output)
-
-    import timeit
-    setup = 'from __main__ import random_words'
-    print(timeit.timeit('random_words(num_words=10)', setup=setup, number=100))
