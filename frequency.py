6a659ab2e0cd589d73e2ced57a21fd5585fb4767
Lucia Reynoso <rvaicul@gmail.com>
2018-10-30 15:45:45 -0700

    comments

--- frequency.py
+++ frequency.py
@@ -9,7 +9,8 @@ import json
 def alphanumericize(source_text):
     source = source_text.lower()
     source = source.replace('-', ' ')
-    # this is backwards - I should instead filter all but alphanumerics. todo
+    # this is backwards - I should instead filter all but alphanumerics. But Crime & Punishment has
+    # an expanded character set due to its use of phrases in other languages, and digraphs.
     translate_table = str.maketrans(dict.fromkeys(string.punctuation + '\u201c\u201d\u2018\u2019'))
     text = source.translate(translate_table)
     return text
