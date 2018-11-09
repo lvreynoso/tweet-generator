bc777e33dd89976e26d7cdc06652f5fe660e3b48
Lucia Reynoso <rvaicul@gmail.com>
2018-11-09 15:10:38 -0800

    implemented first order markov chain

--- app.py
+++ app.py
@@ -17,15 +17,14 @@ with open(source_path, 'r') as file:
     source = file.read()
 clean_source = cleanup.alphanumericize(source_text=source)
 tokens = tokenate.generate(source_text=clean_source)
-histogram = word_count.generate(token_list=tokens)
+source_map = sample.markov_path(token_list=tokens)
 
 @app.route('/')
 def index():
-    sentence = ''
-    for i in range(10):
-        sentence += ' ' + sample.word(histogram)
+    sentence = sample.markov_walk(path=source_map, distance=10)
     return sentence
 
 if __name__ == '__main__':
-    print('(X) ERROR\nThis program has performed an illegal operation and will be shut down.')
-    print('Do not move. Your IP has been traced and the Internet Police are on their way.')
+    # print('(X) ERROR\nThis program has performed an illegal operation and will be shut down.')
+    # print('Do not move. Your IP has been traced and the Internet Police are on their way.')
+    print(source_map)
--- sample.py
+++ sample.py
@@ -2,6 +2,7 @@
 # sample.py
 # module for generating a sample word from a histogram
 
+import dictogram
 import random
 
 def word(histogram):
@@ -17,3 +18,29 @@ def word(histogram):
             random_weight -= value
     
     return random_word
+
+def markov_path(token_list):
+    markov_map = {}
+    previous_word = None
+    for token in token_list:
+        if token not in markov_map:
+            markov_map[token] = dictogram.Dictogram()
+        if previous_word is not None:
+            markov_map[previous_word].add_count(token)
+        previous_word = token
+    return markov_map
+
+def markov_walk(path, distance):
+    sentence = ''
+    current_word = None
+    for step in range(distance):
+        if current_word is None or len(path[current_word]) == 0:
+            # choose a random word from the dictionary
+            current_word = random.choice(list(path.keys()))
+            sentence += ' ' + current_word
+        else:
+            set_of_possibities = path[current_word]
+            next_word = word(set_of_possibities)
+            sentence += ' ' + next_word
+            current_word = next_word
+    return sentence
